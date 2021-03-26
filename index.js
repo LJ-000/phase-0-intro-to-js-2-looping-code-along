71a61a3f8b0ae623dd6c5e7987983dd83fbe3e35
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-03-26 11:36:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const cards=['Ada', 'Brendan', 'Ali'];
+
+function writeCards(cards) {
+    for (let i = 0; i <cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
+        debugger;
+    }
+    return cards;
+}
+
+writeCards(cards); 
+
+
+const numbers = ["10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+
+function countDown(numbers) {
+  let i = 0; // the initialization moves OUTSIDE the body of the loop!
+  while (i < numbers.length) {
+    console.log(`Countdown ${numbers[i]}`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+  return numbers;
+}
+
+countDown(numbers)
